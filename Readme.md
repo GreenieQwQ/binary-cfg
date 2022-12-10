3364b5936121502a7e2cbe7f3e97049181744d78
Photooon <1527842029@qq.com>
2022-12-10 17:05:32 +0800

    修复节点重复问题

--- Readme.md
+++ Readme.md
@@ -7,12 +7,19 @@ Interactive Visualization for Binary CFG
 ## Todo
 
 - [x] CFG结构提取
+- [ ] 修复edge问题；重新匹配数据结构
 - [ ] 图渲染
 - [ ] 节点聚合与还原
 - [ ] DOI
 - [ ] 语义聚合
 - [ ] 用户框选交互
 - [ ] 节点注释
+
+
+
+## Bug
+
+- [ ] ais3_crackme生成结果与angr_utils提供的示例图略有差异
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import angr
-from bingraphvis.angr import AngrVisFactory
+from bingraphvis.angr import *
 from bingraphvis import DotOutput
 
 import argparse
@@ -9,7 +9,7 @@ import json
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
 def process(vis, obj=None, filter=None):
-    # 自定义process，不适用bingraphvis提供的process
+    # 自定义process，不使用bingraphvis提供的process
     vis.preprocess(obj)
     graph = vis.pipeline.process(filter=filter)
     return graph
@@ -21,49 +21,40 @@ def re_index(nodes, edges):
     for id, n in enumerate(nodes):
         n.id = id
     for e in edges:
-        src_n = e.src
-        dst_n = e.dst
-        src_id = None
-        dst_id = None
-        for i in range(len(nodes)):
-            if src_n == nodes[i]:
-                src_id = i
-            if dst_n == nodes[i]:
-                dst_id = i
-        assert src_id is not None
-        assert dst_id is not None
+        src_id = e.src.id
+        dst_id = e.dst.id
         ri_edges.append((src_id, dst_id))
         adj_list[src_id].append(dst_id)
     return nodes, ri_edges, adj_list
 
-def extract_node_info(node, adj_list):
+def extract_node_info(node):
     info = {}
     info['id'] = node.id
     info['children'] = []
-    info['content'] = {
-        'head': {
+    info['content'] = {}
+    if node.content.get('head', None) is not None:
+        info['content']['head'] = {
             'addr': node.content['head']['data'][0]['addr']['content'],
             'func_addr': node.content['head']['data'][0]['func_addr']['content'],
             'name': node.content['head']['data'][0]['name']['content'],
             'attributes': node.content['head']['data'][0]['attributes']['content']
-        },
-        'asm': [{
+        }
+    if node.content.get('asm', None) is not None:
+        info['content']['asm'] = [{
             'addr': item['addr']['content'],
             'mnemonic': item['mnemonic']['content'],
             'operands': item['operands']['content']
             } for item in node.content['asm']['data']
-        ],
-        'sign': {
-            'has_return': node.obj.has_return,
-            'is_simprocedure': node.obj.is_simprocedure,
-            'is_syscall': node.obj.is_syscall,
-            'no_ret': node.obj.no_ret
-        },
-        'byte_string': str(node.obj.byte_string)
+        ]
+    if node.obj.byte_string is not None:
+        info['content']['byte_string'] = str(node.obj.byte_string)
+    info['content']['sign'] = {
+        'has_return': node.obj.has_return,
+        'is_simprocedure': node.obj.is_simprocedure,
+        'is_syscall': node.obj.is_syscall,
+        'no_ret': node.obj.no_ret
     }
     info['edge'] = []
-    for id in adj_list[node.id]:
-        info['edge'].append(id)
 
     return info
 
@@ -73,8 +64,8 @@ def get_start_id(adj_list):
     out_degree = [0 for _ in range(len(adj_list))]
 
     for id in range(len(adj_list)):
-        out_degree[id] += 1
         for out_id in adj_list[id]:
+            out_degree[id] += 1
             in_degree[out_id] += 1
     
     for id in range(len(adj_list)):
@@ -89,11 +80,14 @@ def get_start_id(adj_list):
 
 def dfs_gen_root(start_id, nodes, adj_list, has_seen):
     root_n = nodes[start_id]
-    root = extract_node_info(root_n, adj_list)
+    root = extract_node_info(root_n)
     has_seen.add(start_id)
 
     for id in adj_list[start_id]:
-        root['children'].append(dfs_gen_root(id, nodes, adj_list, has_seen))
+        if id in has_seen:
+            root['edge'].append(id)
+        else:
+            root['children'].append(dfs_gen_root(id, nodes, adj_list, has_seen))
 
     return root
 
@@ -103,20 +97,24 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--file", metavar="file", type=str, help="the bindary file used to explore")
     args = parser.parse_args()
     binary = args.file
-    main_opts = {}
     
     # Call Angr
-    project = angr.Project(binary, load_options={'auto_load_libs': False, 'main_opts': main_opts})
-    cfg = project.analyses.CFGFast(fail_fast=False, normalize=True, show_progressbar=True,
-                                   symbols=True, function_prologues=True, force_complete_scan=True,
-                                   collect_data_references=False, resolve_indirect_jumps=True)
+    project = angr.Project(binary, load_options={'auto_load_libs': False})
+    main_start = project.loader.main_object.get_symbol('main')
+    start_state = project.factory.blank_state(addr=main_start.rebased_addr)
+    cfg = project.analyses.CFGEmulated(fail_fast=True, starts=[main_start.rebased_addr], initial_state=start_state)
+    # cfg = project.analyses.CFGFast(fail_fast=False, normalize=True, show_progressbar=True,
+    #                                symbols=True, function_prologues=True, force_complete_scan=True,
+    #                                collect_data_references=False, resolve_indirect_jumps=True)
 
-    addr = project.kb.functions['main'].addr
-    vis = AngrVisFactory().default_cfg_pipeline(cfg, asminst=True)
+    vis = AngrVisFactory().default_cfg_pipeline(cfg, asminst=True, remove_path_terminator=True)
+    vis.add_transformer(AngrRemoveImports(cfg.project))
     vis.set_output(DotOutput("test.png", format='svg'))
 
-    cgraph = process(vis, cfg.graph, filter=lambda node: node.obj.function_address == addr)
-    nodes = cgraph.nodes
+    # vis.process(cfg.graph)
+    # cgraph = vis.pipeline.node_annotators[0].graph
+    cgraph = process(vis, cfg.graph)
+    nodes = list(cgraph.nodes)
     edges = cgraph.edges
 
     # Generate Root
